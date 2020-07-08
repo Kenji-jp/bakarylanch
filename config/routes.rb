48a1160a22e5b865e863d62d9c0adf697189f695
kenji <tora169mm@gmail.com>
2020-07-08 17:53:40 +0900

    [add] controller for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'searches/search'
+  end
+  namespace :admins do
+    get 'products/edit'
+    get 'products/index'
+    get 'products/new'
+    get 'products/show'
+  end
+  namespace :admins do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admins do
+    get 'homes/top'
+  end
+  namespace :admins do
+    get 'genres/edit'
+    get 'genres/index'
+  end
+  namespace :admins do
+    get 'customers/edit'
+    get 'customers/index'
+    get 'customers/show'
+  end
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
