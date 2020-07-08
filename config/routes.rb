96d8e1fb987ecbc9b1f8889a2157cf5c7484c7f1
kenji <tora169mm@gmail.com>
2020-07-08 15:22:41 +0900

    [add] controller for customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'orders/confirm'
+  get 'orders/index'
+  get 'orders/new'
+  get 'orders/show'
+  get 'orders/thanks'
+  get 'order_items/index'
+  get 'genres/index'
+  get 'genres/show'
+  get 'customers/edit'
+  get 'customers/show'
+  get 'customers/withdraw'
+  get 'cart_items/index'
+  get 'addresses/edit'
+  get 'addresses/index'
   get 'homes/about'
   get 'homes/top'
   # devise_for :customers
   # devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
