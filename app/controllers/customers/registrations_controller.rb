6763b41ef85b01dfa300ee8cd01868945b395869
kenji <tora169mm@gmail.com>
2020-07-10 11:38:07 +0900

    [add] ログインとログアウト機能

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -3,7 +3,8 @@
 class Customers::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  before_action :authenticate_customer!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # GET /resource/sign_up
   # def new
   #   super
@@ -59,4 +60,19 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    customer_top_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_active, :first_name, :first_name_kana, :family_name, :family_name_kana, :post_code, :address, :tel])
+  end
+
+  protected
+  def update_resouce(resource, parans)
+    resource.update_without_password(params)
+  end
 end
